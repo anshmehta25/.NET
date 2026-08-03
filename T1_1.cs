363dd6657e7c7fde5b1ba16fd14e45f3ef346519
anshmehta25 <anshmehta251@gmail.com>
2026-08-03 10:51:46 +0530

    Add files via upload

---
+++ T1_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorials
+{
+    internal class T1_1
+    {
+        static void Main(string[] args)
+        {"
+            Console.WriteLine("Mehta Ansh - 25SOECE13068);
+
+            Console.WriteLine("hello");
+
+        }
+    }
+}
