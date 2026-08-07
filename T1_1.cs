a090b28279dd7310bac8451c4dd9b627036203ed
anshmehta25 <anshmehta251@gmail.com>
2026-08-07 09:38:21 +0530

    Add files via upload

--- T1_1.cs
+++ T1_1.cs
@@ -7,7 +7,7 @@ namespace Tutorials
     internal class T1_1
     {
         static void Main(string[] args)
-        {"
+        {
             Console.WriteLine("Mehta Ansh - 25SOECE13068);
 
             Console.WriteLine("hello");
---
+++ T1_2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tutorials
+{
+    internal class T1_2
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Pr1 Details");
+            Console.WriteLine("Name : Mehta Ansh");
+            Console.WriteLine("DOB : 25/01/2007");
+            Console.WriteLine("Address :Rajkot ");
+            Console.WriteLine("City : Rajkot");
+            Console.WriteLine("Pincode : 365601");
+            Console.WriteLine("State : Gujarat");
+            Console.WriteLine("Country : India");
+            Console.WriteLine("Email : “amehta688@rku.ac.in");
+        }
+    }
+}
